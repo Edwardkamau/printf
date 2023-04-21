fb4e319a038e526662ee5d1bfeeeaa589f61b965
Makori Nyachaki <nyachakim@gmail.com>
2023-04-21 00:31:35 -0700

    Added edited  main.h

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef PRINTF_PR
 #define PRINTF_PR
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
 	int _printf(const char *format, ...);
 
 #endif /*end ifndef */
